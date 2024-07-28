842f8406dd085c21105fba09d3b7cfb7806f8b18
kshitij0318 <111875622+kshitij0318@users.noreply.github.com>
2024-07-28 17:29:06 +0530

    Add files via upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,8 +10,19 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:8800",
-        changeOrigin: true,  
-      }
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        'events',
+        'url',
+        'http',
+        'path',
+        'buffer'
+      ]
     }
   }
-})
+});
